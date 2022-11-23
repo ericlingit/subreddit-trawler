d02076d3ee1c1b4c7fc6a9fefded955a48e81dcb
ericlingit <ericlingit@users.noreply.github.com>
2022-11-23 16:52:10 +0900

    Return a Content object.

--- post_scraper.py
+++ post_scraper.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List, Optional
+from typing import List, Optional, Tuple
 from urllib import parse
 
 import requests
@@ -13,74 +13,85 @@ from subreddit import PostLink, PostType, headers
 @dataclass
 class Video:
     # Video and audio track are stored separately.
-    video: str
-    audio: str
+    video_track: str
+    audio_track: str
 
 
 @dataclass
 class Content:
-    comment: List[str]
-    image: List[str]  # List of image URLs.
+    title: str
+    flare: str
+    # `comment` has OP's comment and all replies. Reply structure and commentor
+    # metadata are not preserved. Each <p> tag text is a str item in this list.
+    comments: List[str]
+    images: List[str]  # List of image URLs.
     video: Optional[Video]
 
 
-def parse_post_content(soup: BeautifulSoup, metadata: PostLink) -> None:
+def parse_post_content(soup: BeautifulSoup, metadata: PostLink) -> Content:
     # Title.
+    title: str = ""
     maybe_title: List[Tag] = soup.select("p.title a.title")
-    assert len(maybe_title) == 1
-    title = maybe_title.pop()
-    print(title.text.strip())  # Title text could be blank.
+    if len(maybe_title) == 1:
+        title_tag = maybe_title.pop()
+        title = title_tag.text.strip()  # Title text could be blank.
 
     # Flare, if any.
+    flare: str = ""
     maybe_flare: List[Tag] = soup.select("p.title span.linkflairlabel")
-    flare = maybe_flare[0].text.strip() if len(maybe_flare) > 0 else "(no flare)"
-    print(flare)
+    if len(maybe_flare) == 1:
+        flare_tag = maybe_flare.pop()
+        flare = flare_tag.text.strip()
 
-    # # Comments, including opening poster's comment (reply structure not preserved).
-    # comments = soup.select("div.entry div.usertext-body div.md p")
-    # print(len(comments))
-    # for p in comments:
-    #     print(p.text)
-
-    # OP image (get link from PostLink object)
-    if metadata.type is PostType.Image:
-        print(f"found image: {metadata.url}")
+    # Comments, including opening poster's comment (reply structure not preserved).
+    maybe_comments = soup.select("div.entry div.usertext-body div.md p")
+    comments: List[str] = [p_tag.text.strip() for p_tag in maybe_comments]
 
     # OP video (get link from PostLink object)
+    video: Optional[Video] = None
     if metadata.type is PostType.Video:
-        metadata.url
         # Transform from "https://v.redd.it/abc123xyz"
-        # to "https://v.redd.it/4huchegx4x0a1/DASH_720.mp4"
+        # to  "https://v.redd.it/4huchegx4x0a1/DASH_720.mp4"
         # and "https://v.redd.it/4huchegx4x0a1/DASH_audio.mp4"
-        print("found video")
-        print(f"{metadata.url}/DASH_720.mp4")
-        print(f"{metadata.url}/DASH_audio.mp4")
+        video = Video(
+            video_track=f"{metadata.url}/DASH_720.mp4",
+            audio_track=f"{metadata.url}/DASH_audio.mp4",
+        )
 
+    # OP image (get link from PostLink object)
+    images: List[str] = []
+    if metadata.type is PostType.Image:
+        images.append(metadata.url)
     # Gallery.
     if metadata.type is PostType.Gallery:
         gallery_img = soup.select(
             "div.media-gallery div.gallery-tiles div div.gallery-tile-content img.preview"
         )
-        print(f"found gallery with {len(gallery_img)} pics")
         for img in gallery_img:
-            src = img.get("src", "")
+            src: str = img.get("src", "")
             # Transform src URL
             # from "https://preview.redd.it/abcxyz123.jpg?width=108&crop=smart&auto=webp&s=xxxxxxxxxxxxxxxxxxxxxxxxxx"
             # to   "https://i.redd.it/abcxyz123.jpg"
-            d = parse.urlparse(src)
-
-            print(
-                parse.urlunparse(
-                    (
-                        d[0],  # scheme="https"
-                        "i.redd.it",  # netloc="i.redd.it"
-                        d[2],  # path="abcxyz123.jpg"
-                        "",  # params=""
-                        "",  # query=""
-                        "",  # fragment=""
-                    )
+            scheme, _, path, _, _, _ = parse.urlparse(src)
+            img_url: str = parse.urlunparse(
+                (
+                    scheme,
+                    "i.redd.it",  # netloc
+                    path,
+                    "",  # params
+                    "",  # query
+                    "",  # fragment
                 )
             )
+            images.append(img_url)
+
+    return Content(
+        title=title,
+        flare=flare,
+        comments=comments,
+        images=images,
+        video=video,
+    )
 
 
 if __name__ == "__main__":
